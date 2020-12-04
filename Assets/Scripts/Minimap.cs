988729f5e9c21246335d6998cd4d02e9e99ab88a
AdamKormos <49873113+AdamKormos@users.noreply.github.com>
2020-12-04 22:26:36 +0100

    Progression Log Day 14
    - Finished minimap

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -10,36 +10,57 @@ public class Minimap : MonoBehaviour
     [SerializeField] GameObject sampleRoomImage = default;
     Vector2 containerBottomLeftPosition = default;
     Vector2 roomImageOffsets = default;
-    List<List<Image>> images = new List<List<Image>>();
+    Image[][] images = default;
+    bool[][] visitedRooms = default;
+    int prevI = -1, prevJ = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        containerBottomLeftPosition = new Vector3(-imageContainerTransform.rect.width / 2f, -imageContainerTransform.rect.width / 2f);
+        images = new Image[MapManager.s_gridSize.y][];
+        visitedRooms = new bool[MapManager.s_gridSize.y][];
+
+        containerBottomLeftPosition = new Vector3(-imageContainerTransform.rect.width / 2f, imageContainerTransform.rect.width / 2f);
         roomImageOffsets = new Vector2(5f + sampleRoomImage.GetComponent<RectTransform>().rect.width, 5f + sampleRoomImage.GetComponent<RectTransform>().rect.height);
         CreateMinimap();
     }
 
     private void Update()
     {
-        imageContainerTransform.anchoredPosition =
-                new Vector2(Player.currentRoomJ * -roomImageOffsets.x, Player.currentRoomI * -roomImageOffsets.y);
-        images[Player.currentRoomI][Player.currentRoomJ].color = Color.blue;
+        if(Player.currentRoomI != prevI || Player.currentRoomJ != prevJ)
+        {
+            OnRoomChanged();
+        }
+
+        prevI = Player.currentRoomI;
+        prevJ = Player.currentRoomJ;
+    }
+
+    private void OnRoomChanged()
+    {
+        int offsetI = Player.currentRoomI - prevI, offsetJ = Player.currentRoomJ - prevJ;
+        if(prevI != -1 && prevJ != -1) images[prevI][prevJ].color = Color.white;
+        //imageContainerTransform.transform.localPosition += new Vector3(jAmount * -55f, iAmount * 70f);
+        imageContainerTransform.offsetMin += new Vector2(offsetJ * -55f, -offsetI * 70f);
+        imageContainerTransform.offsetMax += new Vector2(offsetJ * -55f, -offsetI * 70f);
+
+        images[prevI + offsetI][prevJ + offsetJ].color = Color.blue;
+        images[prevI + offsetI][prevJ + offsetJ].gameObject.SetActive(true);
+        visitedRooms[prevI + offsetI][prevJ + offsetJ] = true;
     }
 
     private void CreateMinimap()
     {
-        for(int i = 0; i < MapManager.s_gridSize.x; i++)
+        for(int i = 0; i < MapManager.s_gridSize.y; i++)
         {
-            images.Add(new List<Image>());
-            for(int j = 0; j < MapManager.s_gridSize.y; j++)
+            images[i] = new Image[MapManager.s_gridSize.x];
+            visitedRooms[i] = new bool[MapManager.s_gridSize.x];
+            for(int j = 0; j < MapManager.s_gridSize.x; j++)
             {
-                if(MapManager.roomGrid[i][j] != null)
-                {
-                    GameObject roomImage = Instantiate(sampleRoomImage, sampleRoomImage.transform.position, Quaternion.identity, imageContainerTransform.transform);
-                    roomImage.transform.localPosition = containerBottomLeftPosition + new Vector2(j * roomImageOffsets.x, i * roomImageOffsets.y);
-                    images[i].Add(roomImage.GetComponent<Image>());
-                }
+                GameObject roomImage = Instantiate(sampleRoomImage, sampleRoomImage.transform.position, Quaternion.identity, imageContainerTransform.transform);
+                roomImage.transform.localPosition = new Vector2(j * roomImageOffsets.x, i * roomImageOffsets.y) - containerBottomLeftPosition;
+                images[i][j] = roomImage.GetComponent<Image>();
+                roomImage.SetActive(false);
             }
         }
 
